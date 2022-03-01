118553e9319a5d5739463fabc5242b797f9e81e3
Jerome Tan <jerome.tan@tuta.io>
2022-03-01 18:17:56 +0100

    installed pundit. applied authorization policy on House

---
+++ app/policies/house_policy.rb
@@ -0,0 +1,33 @@
+class HousePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      # by default: give access to all houses for the index
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def index?
+    user.admin?
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def edit?
+    update?
+  end
+end
