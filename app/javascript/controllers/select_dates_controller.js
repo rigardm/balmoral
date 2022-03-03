1c26f6feaf57eb03f28df951ef11b7f886171abf
Florent Geron <florent.geron@orange.fr>
2022-03-03 12:47:24 +0100

    today et  proposed meetings passés au format bubulle

--- app/javascript/controllers/select_dates_controller.js
+++ app/javascript/controllers/select_dates_controller.js
@@ -3,14 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["date", "body", "button"]
 
+  connect() {
+    const today = document.querySelector('[class*="today"]');
+    const day_display = parseInt(today.dataset.date.slice(-2), 10);
+    today.insertAdjacentHTML('beforeend', `<div class="day-today">${day_display}</div>`);
+  }
+
+
   select(event) {
     if (this.bodyTarget.dataset.click === 'arrival') {
       this.buttonTarget.classList.add('d-none');
       this.dateTargets.forEach((date)=>{
         date.classList.remove("new-booking");
+        if (date.lastChild.className === "meeting-proposed") {
+          date.removeChild(date.lastChild);
+        };
+        //.forEach(element => element.remove);
       });
       const arrival = event.currentTarget;
       arrival.classList.add("new-booking");
+      const day_display = parseInt(arrival.dataset.date.slice(-2), 10);
+      arrival.insertAdjacentHTML('beforeend', `<div class="meeting-proposed">${day_display}</div>`);
       this.bodyTarget.dataset.click = 'departure';
     } else {
       const arrival = document.querySelector('.new-booking')
@@ -21,6 +34,8 @@ export default class extends Controller {
         this.dateTargets.forEach((date)=>{
           if ((date.dataset.date>arrivalDate)&&(date.dataset.date<=departureDate)) {
             date.classList.add("new-booking");
+           const day_display = parseInt(date.dataset.date.slice(-2), 10);
+           date.insertAdjacentHTML('beforeend', `<div class="meeting-proposed">${day_display}</div>`);
           }
         });
         this.bodyTarget.dataset.arrivalDate = arrivalDate;
@@ -29,6 +44,7 @@ export default class extends Controller {
       } else {
         // Departure is before arrival, we need to reset the calendar
         arrival.classList.remove('new-booking');
+          arrival.removeChild(arrival.lastChild);
       }
       this.bodyTarget.dataset.click = 'arrival';
     }
