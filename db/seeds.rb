e30dfc3043675c5c21546dde2246591c6c08de85
Jerome Tan <jerome.tan@tuta.io>
2022-03-04 12:18:06 +0100

    déplacé les ajustements sur booking avant save du controller vers le modele

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,18 +29,18 @@ tribu_verte = Tribe.create!(
   shareholding: 0.25,
   house: chaumiere
 )
-tribu_rose = Tribe.create!(
-  credits: 250,
-  color: "tribe-red",
-  shareholding: 0.25,
-  house: chaumiere
-)
-tribu_bleue = Tribe.create!(
-  credits: 500,
-  color: "tribe-blue",
-  shareholding: 0.50,
-  house: chaumiere
-)
+# tribu_rose = Tribe.create!(
+#   credits: 250,
+#   color: "tribe-red",
+#   shareholding: 0.25,
+#   house: chaumiere
+# )
+# tribu_bleue = Tribe.create!(
+#   credits: 500,
+#   color: "tribe-blue",
+#   shareholding: 0.50,
+#   house: chaumiere
+# )
 puts "created #{Tribe.count} #{'tribe'.pluralize(Tribe.count)}"
 
 puts "4 out of 8: SEED USERS"
@@ -92,260 +92,279 @@ file = URI.open('https://i.pravatar.cc/300?img=53')
 oscar.photo.attach(io: file, filename: 'oscar.jpg', content_type: 'image/jpg')
 oscar.save!
 
-jacques = User.new(
-  email: 'jacques@jacques.com',
-  password: '000000',
-  first_name: 'Jacques',
-  last_name: 'Perron',
-  role: :admin,
-  tribe: tribu_rose
-)
-file = URI.open('https://i.pravatar.cc/300?img=50')
-jacques.photo.attach(io: file, filename: 'jacques.jpg', content_type: 'image/jpg')
-jacques.save!
+# jacques = User.new(
+#   email: 'jacques@jacques.com',
+#   password: '000000',
+#   first_name: 'Jacques',
+#   last_name: 'Perron',
+#   role: :admin,
+#   tribe: tribu_rose
+# )
+# file = URI.open('https://i.pravatar.cc/300?img=50')
+# jacques.photo.attach(io: file, filename: 'jacques.jpg', content_type: 'image/jpg')
+# jacques.save!
 
-elsa = User.new(
-  email: 'elsa@elsa.com',
-  password: '000000',
-  first_name: 'Elsa',
-  last_name: 'Perron',
-  role: :member,
-  tribe: tribu_rose
-)
-file = URI.open('https://i.pravatar.cc/300?img=44')
-elsa.photo.attach(io: file, filename: 'elsa.jpg', content_type: 'image/jpg')
-elsa.save!
+# elsa = User.new(
+#   email: 'elsa@elsa.com',
+#   password: '000000',
+#   first_name: 'Elsa',
+#   last_name: 'Perron',
+#   role: :member,
+#   tribe: tribu_rose
+# )
+# file = URI.open('https://i.pravatar.cc/300?img=44')
+# elsa.photo.attach(io: file, filename: 'elsa.jpg', content_type: 'image/jpg')
+# elsa.save!
 
-nathalie = User.new(
-  email: 'nathalie@nathalie.com',
-  password: '000000',
-  first_name: 'Nath',
-  last_name: 'Rousseau',
-  role: :admin,
-  tribe: tribu_bleue
-)
-file = URI.open('https://i.pravatar.cc/300?img=39')
-nathalie.photo.attach(io: file, filename: 'nathalie.jpg', content_type: 'image/jpg')
-nathalie.save!
+# nathalie = User.new(
+#   email: 'nathalie@nathalie.com',
+#   password: '000000',
+#   first_name: 'Nath',
+#   last_name: 'Rousseau',
+#   role: :admin,
+#   tribe: tribu_bleue
+# )
+# file = URI.open('https://i.pravatar.cc/300?img=39')
+# nathalie.photo.attach(io: file, filename: 'nathalie.jpg', content_type: 'image/jpg')
+# nathalie.save!
 
-nicolas = User.new(
-  email: 'nicolas@nicolas.com',
-  password: '000000',
-  first_name: 'Nico',
-  last_name: 'Rousseau',
-  role: :member,
-  tribe: tribu_bleue
-)
-file = URI.open('https://i.pravatar.cc/300?img=60')
-nicolas.photo.attach(io: file, filename: 'nicolas.jpg', content_type: 'image/jpg')
-nicolas.save!
+# nicolas = User.new(
+#   email: 'nicolas@nicolas.com',
+#   password: '000000',
+#   first_name: 'Nico',
+#   last_name: 'Rousseau',
+#   role: :member,
+#   tribe: tribu_bleue
+# )
+# file = URI.open('https://i.pravatar.cc/300?img=60')
+# nicolas.photo.attach(io: file, filename: 'nicolas.jpg', content_type: 'image/jpg')
+# nicolas.save!
 
-jeremy = User.new(
-  email: 'jeremy@jeremy.com',
-  password: '000000',
-  first_name: 'Jérém',
-  last_name: 'Rousseau',
-  role: :member,
-  tribe: tribu_bleue
-)
-file = URI.open('https://i.pravatar.cc/300?img=52')
-jeremy.photo.attach(io: file, filename: 'jeremy.jpg', content_type: 'image/jpg')
-jeremy.save!
+# jeremy = User.new(
+#   email: 'jeremy@jeremy.com',
+#   password: '000000',
+#   first_name: 'Jérém',
+#   last_name: 'Rousseau',
+#   role: :member,
+#   tribe: tribu_bleue
+# )
+# file = URI.open('https://i.pravatar.cc/300?img=52')
+# jeremy.photo.attach(io: file, filename: 'jeremy.jpg', content_type: 'image/jpg')
+# jeremy.save!
 
 puts "created #{User.count} users including #{User.admins.count} admins"
 
+puts "la tribu verte a #{tribu_verte.credits} clefs avant seed booking"
+
 puts "5 out of 8: SEED BOOKINGS"
+# Booking.create!(
+#   arrival: arrival = Date.new(2022, 3, 12),
+#   departure: departure = Date.new(2022, 3, 13),
+#   total_price: (departure - arrival) * chaumiere.daily_price,
+#   status: :validated,
+#   house: chaumiere,
+#   user: nicolas
+# )
+
+# Booking.create!(
+  #   arrival: arrival = Date.new(2022, 4, 16),
+  #   departure: departure = Date.new(2022, 4, 23),
+  #   total_price: (departure - arrival) * chaumiere.daily_price,
+  #   status: :validated,
+  #   house: chaumiere,
+  #   user: elsa
+  # )
+
+  Booking.create!(
+    arrival: arrival = Date.new(2022, 5, 7),
+    departure: departure = Date.new(2022, 5, 8),
+    total_price: (departure - arrival) * chaumiere.daily_price,
+    status: :pending,
+    house: chaumiere,
+    user: laure
+  )
+
+  puts "la tribu verte a #{tribu_verte.credits} clefs (expected: 250-10 = 240)"
+
+# Booking.create!(
+#   arrival: arrival = Date.new(2022, 5, 26),
+#   departure: departure = Date.new(2022, 5, 29),
+#   total_price: (departure - arrival) * chaumiere.daily_price,
+#   status: :validated,
+#   house: chaumiere,
+#   user: jeremy
+# )
+# Booking.create!(
+#   arrival: arrival = Date.new(2022, 6, 11),
+#   departure: departure = Date.new(2022, 6, 18),
+#   total_price: (departure - arrival) * chaumiere.daily_price,
+#   status: :validated,
+#   house: chaumiere,
+#   user: nathalie
+# )
+# Booking.create!(
+#   arrival: arrival = Date.new(2022, 7, 2),
+#   departure: departure = Date.new(2022, 7, 3),
+#   total_price: (departure - arrival) * chaumiere.daily_price,
+#   status: :pending,
+#   house: chaumiere,
+#   user: jeremy
+# )
+
 Booking.create!(
-  arrival: arrival = Date.new(2022, 3, 12),
-  departure: departure = Date.new(2022, 3, 13),
-  total_price: (departure - arrival) * chaumiere.daily_price,
-  status: :validated,
-  house: chaumiere,
-  user: nicolas
-)
-Booking.create!(
-  arrival: arrival = Date.new(2022, 4, 16),
-  departure: departure = Date.new(2022, 4, 23),
-  total_price: (departure - arrival) * chaumiere.daily_price,
-  status: :validated,
-  house: chaumiere,
-  user: elsa
-)
-Booking.create!(
-  arrival: arrival = Date.new(2022, 5, 7),
-  departure: departure = Date.new(2022, 5, 8),
+  arrival: arrival = Date.new(2022, 1, 1),
+  departure: departure = Date.new(2022, 1, 15),
   total_price: (departure - arrival) * chaumiere.daily_price,
   status: :pending,
   house: chaumiere,
-  user: laure
+  user: michel
 )
-Booking.create!(
-  arrival: arrival = Date.new(2022, 5, 26),
-  departure: departure = Date.new(2022, 5, 29),
-  total_price: (departure - arrival) * chaumiere.daily_price,
-  status: :validated,
-  house: chaumiere,
-  user: jeremy
-)
-Booking.create!(
-  arrival: arrival = Date.new(2022, 6, 11),
-  departure: departure = Date.new(2022, 6, 18),
-  total_price: (departure - arrival) * chaumiere.daily_price,
-  status: :validated,
-  house: chaumiere,
-  user: nathalie
-)
-Booking.create!(
-  arrival: arrival = Date.new(2022, 7, 2),
-  departure: departure = Date.new(2022, 7, 3),
-  total_price: (departure - arrival) * chaumiere.daily_price,
-  status: :pending,
-  house: chaumiere,
-  user: jeremy
-)
+
+puts "la tribu verte a #{tribu_verte.credits} clefs (expected: 240 - 75 = 165)"
+
 puts "created #{Booking.count} #{'booking'.pluralize(Booking.count)}"
 
-puts "6 out of 8: SEED SPENDINGS"
-Spending.create!(
-  amount: 96,
-  name: "Femme de ménage",
-  category: 'Entretien',
-  date: Date.new(2022, 1, 3),
-  details: '',
-  tribe: tribu_verte
-)
-Spending.create!(
-  amount: 753,
-  name: "Taxe foncière",
-  category: 'Admin',
-  date: Date.new(2022, 1, 15),
-  details: 'Admin',
-  tribe: tribu_bleue
-)
-Spending.create!(
-  amount: 160,
-  name: "Plombier",
-  category: 'Entretien',
-  date: Date.new(2022, 1, 20),
-  details: "Problème de fuite sur l'évacuation du lave-vaisselle",
-  tribe: tribu_bleue
-)
-Spending.create!(
-  amount: 3500,
-  name: "Remplacement chaudière",
-  category: 'Travaux',
-  date: Date.new(2022, 1, 31),
-  details: 'Chaudière HS. Remplacement obligatoire. Nouveau modèle avec thermostat variable.',
-  tribe: tribu_bleue
-)
-Spending.create!(
-  amount: 96,
-  name: "Femme de ménage",
-  category: 'Entretien',
-  date: Date.new(2022, 2, 5),
-  details: '',
-  tribe: tribu_verte
-)
-Spending.create!(
-  amount: 80,
-  name: "Jardinier",
-  category: 'Entretien',
-  date: Date.new(2022, 2, 25),
-  details: '',
-  tribe: tribu_bleue
-)
-Spending.create!(
-  amount: 96,
-  name: "Femme de ménage",
-  category: 'Entretien',
-  date: Date.new(2022, 3, 2),
-  details: '',
-  tribe: tribu_verte
-)
-Spending.create!(
-  amount: 80,
-  name: "Electricité janvier",
-  category: 'Charges',
-  date: Date.new(2022, 1, 10),
-  details: '',
-  tribe: tribu_bleue
-)
-Spending.create!(
-  amount: 80,
-  name: "Electricité février",
-  category: 'Charges',
-  date: Date.new(2022, 2, 10),
-  details: '',
-  tribe: tribu_bleue
-)
-Spending.create!(
-  amount: 80,
-  name: "Electricité mars",
-  category: 'Charges',
-  date: Date.new(2022, 3, 10),
-  details: '',
-  tribe: tribu_bleue
-)
-puts "created #{Spending.count} #{'spending'.pluralize(Spending.count)}"
+# puts "6 out of 8: SEED SPENDINGS"
+# Spending.create!(
+#   amount: 96,
+#   name: "Femme de ménage",
+#   category: 'Entretien',
+#   date: Date.new(2022, 1, 3),
+#   details: '',
+#   tribe: tribu_verte
+# )
+# Spending.create!(
+#   amount: 753,
+#   name: "Taxe foncière",
+#   category: 'Admin',
+#   date: Date.new(2022, 1, 15),
+#   details: 'Admin',
+#   tribe: tribu_bleue
+# )
+# Spending.create!(
+#   amount: 160,
+#   name: "Plombier",
+#   category: 'Entretien',
+#   date: Date.new(2022, 1, 20),
+#   details: "Problème de fuite sur l'évacuation du lave-vaisselle",
+#   tribe: tribu_bleue
+# )
+# Spending.create!(
+#   amount: 3500,
+#   name: "Remplacement chaudière",
+#   category: 'Travaux',
+#   date: Date.new(2022, 1, 31),
+#   details: 'Chaudière HS. Remplacement obligatoire. Nouveau modèle avec thermostat variable.',
+#   tribe: tribu_bleue
+# )
+# Spending.create!(
+#   amount: 96,
+#   name: "Femme de ménage",
+#   category: 'Entretien',
+#   date: Date.new(2022, 2, 5),
+#   details: '',
+#   tribe: tribu_verte
+# )
+# Spending.create!(
+#   amount: 80,
+#   name: "Jardinier",
+#   category: 'Entretien',
+#   date: Date.new(2022, 2, 25),
+#   details: '',
+#   tribe: tribu_bleue
+# )
+# Spending.create!(
+#   amount: 96,
+#   name: "Femme de ménage",
+#   category: 'Entretien',
+#   date: Date.new(2022, 3, 2),
+#   details: '',
+#   tribe: tribu_verte
+# )
+# Spending.create!(
+#   amount: 80,
+#   name: "Electricité janvier",
+#   category: 'Charges',
+#   date: Date.new(2022, 1, 10),
+#   details: '',
+#   tribe: tribu_bleue
+# )
+# Spending.create!(
+#   amount: 80,
+#   name: "Electricité février",
+#   category: 'Charges',
+#   date: Date.new(2022, 2, 10),
+#   details: '',
+#   tribe: tribu_bleue
+# )
+# Spending.create!(
+#   amount: 80,
+#   name: "Electricité mars",
+#   category: 'Charges',
+#   date: Date.new(2022, 3, 10),
+#   details: '',
+#   tribe: tribu_bleue
+# )
+# puts "created #{Spending.count} #{'spending'.pluralize(Spending.count)}"
 
-puts "7 out of 8: SEED CHANNELS"
-general = Channel.create!(
-  name: "general",
-  house: chaumiere
-)
-puts "created #{Channel.count} #{'channel'.pluralize(Channel.count)}"
+# puts "7 out of 8: SEED CHANNELS"
+# general = Channel.create!(
+#   name: "general",
+#   house: chaumiere
+# )
+# puts "created #{Channel.count} #{'channel'.pluralize(Channel.count)}"
 
-puts "8 out of 8: SEED MESSAGES"
-Message.create!(
-  content: "Salut la familia! La chaudière était kaput, j'ai du la faire remplacer. Ca douille un peu: 3500 balles!",
-  user: jacques,
-  channel: general
-)
-Message.create!(
-  content: "Jacques, on sera bien là à 10h samedi pour récupérer les clefs",
-  user: nathalie,
-  channel: general
-)
-Message.create!(
-  content: "Super weekend avec les enfants qui ont fait leur premier stage de kite.",
-  user: sophie,
-  channel: general
-)
-Message.create!(
-  content: "Le jardinier a planté 3 nouveaux érables du côté de la palissade.",
-  user: nathalie,
-  channel: general
-)
-Message.create!(
-  content: "J'ai changé la photo de la piscine sur AirBNB",
-  user: michel,
-  channel: general
-)
-Message.create!(
-  content: "Bon anniv Nico!!! (de la part de TOUTES les cousines)",
-  user: elsa,
-  channel: general
-)
-Message.create!(
-  content: "Oups! J'ai cassé le filet de badminton. J'en rapporte un neuf à l'Ascension.",
-  user: jeremy,
-  channel: general
-)
-Message.create!(
-  content: "Quelqu'un saurait où est le double du cadenas du tandem?",
-  user: nicolas,
-  channel: general
-)
-Message.create!(
-  content: "Dans ton casier",
-  user: oscar,
-  channel: general
-)
-Message.create!(
-  content: "@Laure: il y a un stage de tir à l'arc au Croquan pendant les vacances de Pâques",
-  user: nicolas,
-  channel: general
-)
-puts "created #{Message.count} #{'message'.pluralize(Message.count)}"
+# puts "8 out of 8: SEED MESSAGES"
+# Message.create!(
+#   content: "Salut la familia! La chaudière était kaput, j'ai du la faire remplacer. Ca douille un peu: 3500 balles!",
+#   user: jacques,
+#   channel: general
+# )
+# Message.create!(
+#   content: "Jacques, on sera bien là à 10h samedi pour récupérer les clefs",
+#   user: nathalie,
+#   channel: general
+# )
+# Message.create!(
+#   content: "Super weekend avec les enfants qui ont fait leur premier stage de kite.",
+#   user: sophie,
+#   channel: general
+# )
+# Message.create!(
+#   content: "Le jardinier a planté 3 nouveaux érables du côté de la palissade.",
+#   user: nathalie,
+#   channel: general
+# )
+# Message.create!(
+#   content: "J'ai changé la photo de la piscine sur AirBNB",
+#   user: michel,
+#   channel: general
+# )
+# Message.create!(
+#   content: "Bon anniv Nico!!! (de la part de TOUTES les cousines)",
+#   user: elsa,
+#   channel: general
+# )
+# Message.create!(
+#   content: "Oups! J'ai cassé le filet de badminton. J'en rapporte un neuf à l'Ascension.",
+#   user: jeremy,
+#   channel: general
+# )
+# Message.create!(
+#   content: "Quelqu'un saurait où est le double du cadenas du tandem?",
+#   user: nicolas,
+#   channel: general
+# )
+# Message.create!(
+#   content: "Dans ton casier",
+#   user: oscar,
+#   channel: general
+# )
+# Message.create!(
+#   content: "@Laure: il y a un stage de tir à l'arc au Croquan pendant les vacances de Pâques",
+#   user: nicolas,
+#   channel: general
+# )
+# puts "created #{Message.count} #{'message'.pluralize(Message.count)}"
 
 puts "DONE SEEDING. HAVE A GOOD DAY!"
