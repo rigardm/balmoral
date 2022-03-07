3bd70dd4e66dd4f0c4ce3fb75bc61700a0ea1e43
Florent Geron <florent.geron@orange.fr>
2022-03-07 10:34:42 +0100

    passage sur le controller channels et chgt des liens

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,6 @@ class MessagesController < ApplicationController
   before_action :set_channel
   before_action :set_house
 
-
-  def index
-    @messages = policy_scope(Message).order(created_at: :asc)
-    @message = Message.new
-  end
-
   def new
     @message = Message.new
     authorize @message
@@ -19,7 +13,7 @@ class MessagesController < ApplicationController
       @message.channel = @channel
       @message.user = current_user
       @message.save
-      redirect_to house_channel_messages_path(@house, @channel, :anchor => "last")
+      redirect_to house_channel_path(@house, @channel, :anchor => "last")
   end
 
   private
