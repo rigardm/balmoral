38d9be48c84ac88c4cc42c13e439a98e2f2216e2
Florent Geron <florent.geron@orange.fr>
2022-03-10 15:04:00 +0100

    no effects

--- app/javascript/components/keyboard.js
+++ app/javascript/components/keyboard.js
@@ -1,14 +1,17 @@
 const hideNavbarOnKeyboardShow = () => {
-  document.addEventListener('focusin', (e) => {
-   document.querySelector('.bottom-bar').classList.add("d-none");
-   document.querySelector('.message-input').style.paddingBottom = '0vh' ;
-   window.scrollTo(0,document.body.scrollHeight);
-  });
-  document.addEventListener('focusout', (e) => {
-    document.querySelector('.bottom-bar').classList.remove("d-none");
-    document.querySelector('.message-input').style.paddingBottom = '13vh';
+  const messageInput = document.querySelector('.message-input')
+  if (messageInput) {
+    document.addEventListener('focusin', (e) => {
+    document.querySelector('.bottom-bar').classList.add("d-none");
+    messageInput.style.paddingBottom = '0vh' ;
     window.scrollTo(0,document.body.scrollHeight);
-  });
+    });
+    document.addEventListener('focusout', (e) => {
+      document.querySelector('.bottom-bar').classList.remove("d-none");
+      messageInput.style.paddingBottom = '13vh';
+      window.scrollTo(0,document.body.scrollHeight);
+    });
+  }
 }
 
 export {hideNavbarOnKeyboardShow}
