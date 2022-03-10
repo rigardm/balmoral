93a36f69ccc38be484489323e2676db064255da5
Florent Geron <florent.geron@orange.fr>
2022-03-10 10:32:36 +0100

    solved

--- app/javascript/controllers/channel_subscription_controller.js
+++ app/javascript/controllers/channel_subscription_controller.js
@@ -2,13 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 import consumer from "../channels/consumer"
 
 export default class extends Controller {
-  static values = { channelId: Number }
+  static values = { channelId: Number, userId: Number }
   static targets = ["messages", "form"]
 
   connect() {
     this.channel = consumer.subscriptions.create(
       { channel: "ChannelChannel", id: this.channelIdValue },
-      { received: data => this._insertMessageScrollDownAndResetForm(data) }
+      { received:  (data) => {
+        const htmlNode = document.createElement("div")
+        // htmlNode.insertAdjacentHTML("beforeend", data.html)
+        htmlNode.insertAdjacentHTML("beforeend", data.html)
+        console.log(htmlNode.innerHTML)
+        if (this.userSender(data)) {
+          htmlNode.querySelector("#author-name").innerText = "Vous  "
+        }
+        this._insertMessageScrollDownAndResetForm(htmlNode.firstElementChild)
+      }
+       }
     )
     console.log(`Subscribed to the channel with the id ${this.channelIdValue}.`)
     console.log(this.channel)
@@ -19,8 +29,12 @@ export default class extends Controller {
     this.channel.unsubscribe()
   }
 
+  userSender(data) {
+    return data.user_id === this.userIdValue
+  }
+
   _insertMessageScrollDownAndResetForm(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.appendChild(data)
     window.scrollTo(0,document.body.scrollHeight);
     //this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
     this.formTarget.reset()
